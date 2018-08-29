0eda63edfdae0ffdab4f8fe84966be0f61065c0d
ianmclane15 <ian.mclane@me.com>
2018-08-29 08:51:15 -0700

    Added switch & button to layout Added OnCheckedChangedListener to switch

--- app/src/main/java/com/example/uselessmachine/UselessMachine.java
+++ app/src/main/java/com/example/uselessmachine/UselessMachine.java
@@ -1,7 +1,9 @@
 package com.example.uselessmachine;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
@@ -11,6 +13,8 @@ public class UselessMachine extends AppCompatActivity {
     private Button buttonSelfDestruct;
     private Switch switchUseless;
 
+    public static final String TAG = UselessMachine.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +23,8 @@ public class UselessMachine extends AppCompatActivity {
         wireWidgets();
         setListeners();
     }
+
+
 
     private void setListeners() {
         // TODO self destruct button
@@ -36,8 +42,28 @@ public class UselessMachine extends AppCompatActivity {
         });
     }
 
+    private void startSwitchOffTimer() {
+        CountDownTimer countDownTimer = new CountDownTimer(5000, 5000) {
+            public void onTick(long l) {
+                if (!switchUseless.isChecked()) {
+                    Log.d(TAG, "OnTick: canceling");
+                }
+            }
+
+            @Override
+            public void onFinish() {
+                switchUseless.setChecked(false);
+                Log.d(TAG, "onFinish: switch set to false");
+                cancel();
+            }
+
+        }.start();
+
+
+    }
+
     private void wireWidgets() {
-         buttonSelfDestruct = findViewById(R.id.button_main_selfDestrct);
-         switchUseless = findViewById(R.id.switch_main_useless);
+        buttonSelfDestruct = findViewById(R.id.button_main_selfDestrct);
+        switchUseless = findViewById(R.id.switch_main_useless);
     }
 }
